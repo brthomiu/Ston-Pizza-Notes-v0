bb0370c71ade53a4e6884eb4e4757236164efeb8
Brad <brthomiu@gmail.com>
2023-04-05 12:47:16 -0600

    Remove mailer from main branch

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,19 +1,11 @@
 import express, { Request, Response } from "express";
-import { createUserHandler } from "../controllers/userController";
-import validateResource from "../middleware/validateResource";
-import { createUserSchema } from "../schema/userSchema";
-
 
 const router = express.Router();
 
-router.post(
-  "/api/users",
-  validateResource(createUserSchema),
-  createUserHandler
-);
+router.post("/api/users");
 
 router.get("/api/users", (req: Request, res: Response) => {
-    res.send("This is not the way to log in!!")
-}) 
+  res.send("This is not the way to log in!!");
+});
 
 export default router;
--- server/services/userService.ts
+++
@@ -1,7 +0,0 @@
-import UserModel, { User } from "../models/userModel";
-
-// Create user function, used by userController
-
-export function createUser(input: Partial<User>) {
-  return UserModel.create(input);
-}
